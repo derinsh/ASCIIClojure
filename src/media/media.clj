41aff92ffeb165552385d7db35db4999e8e6f4ac
derinsh <psisisu@gmail.com>
2022-12-22 05:40:09 +0100

    Initial gif support, main.media fixes

--- src/media/media.clj
+++ src/media/media.clj
@@ -7,15 +7,12 @@
    [java.awt Color]
    [javax.imageio ImageIO]
    [javax.imageio.stream FileImageInputStream]
-   [javax.imageio ImageReader]
-   [javax.imageio IIOImage]
-   [java.util Iterator]
    [java.io File]))
 
 ;; IO
 
 (defn file-in
-  "Attempts to load a file from disk and return a File object."
+  "Attempts to load a file from disk and return a `File` object."
   [filename]
   (try
   (^File io/file filename)
@@ -23,57 +20,65 @@
      (println "File could not be read: " (.getMessage e)))))
 
 (defn get-format
-  "Identifies a file extension from string."
+  "Identifies a file extension from a path or filename string."
   [file]
   (last (split file #"\.")))
 
 ;; Image
 
 (defn decode-image
-  "Reads a File and returns a BufferdImage."
+  "Reads a file and returns a `BufferedImage`."
   [^File file]
   (^BufferedImage ImageIO/read file))
 
 ;; Gif
 
 (defn new-stream
-  "Constructs and returns a new FileImageInputStream"
+  "Constructs and returns a new `FileImageInputStream` of a file."
   [file]
   (new FileImageInputStream file))
 
 (defn gif-reader
-  "Returns a .gif ImageReader."
-  [stream]
+  "Takes a `FileImageInputStream` and returns a `GIFImageReader` containing the stream."
+  [^FileImageInputStream stream]
   (let [^com.sun.imageio.plugins.gif.GIFImageReader reader (-> (ImageIO/getImageReaders stream) (.next))]
     (.setInput reader stream)
     reader))
 
 (defn gif-decoder
-  "Returns a GIFImageReader."
+  "Reads a gif file and returns a `GIFImageReader`."
   [^File file]
   (let [^FileImageInputStream stream (new-stream file)]
     ^com.sun.imageio.plugins.gif.GIFImageReader (gif-reader stream)))
 
 ;; RGB frame
 
-(defn recursive--frame
-  "Reads 24-bit RGBA values of an image and returns a matrix of 8-bit R, G and B channels as vectors."
-  [w h ^BufferedImage image]
-  (loop [x 0
-         y 0
-         matrix [[]]]
-    (if (and (= x (dec w)) (= y (dec h)))
-      matrix
-      (let [pixel (^Integer .getRGB image x y)
-            color (new Color pixel)
-            rgb (vector (.getRed color) (.getGreen color) (.getBlue color))]
-        (if (= x (dec w))
-          (recur 0 (inc y) (conj matrix (vector rgb)))
-          (recur (inc x) y (conj (pop matrix) (conj (peek matrix) rgb))))))))
+(defn rgb-vector
+  "Takes a 24-bit RGBA integer and returns a vector of R, G and B values."
+  [rgba]
+  (let [^Color color (new Color rgba)]
+    (vector (.getRed color) (.getGreen color) (.getBlue color))))
 
 (defn get-frame
-  "Takes a BufferedImage and returns a matrix of R, G and B vectors."
-  [image]
+  "Recursively reads 24-bit RGBA values of a `BufferedImage` and returns a frame as a matrix of each pixel as a vector containing 8-bit R, G and B channels."
+  [^BufferedImage image]
+
   (let [width (.getWidth image)
         height (.getHeight image)]
-    (recursive--frame width height image)))
+
+   (loop [x 0
+         y 0
+         frame [[]]]
+
+    (if (and (= x (dec width)) (= y (dec height)))
+      frame
+
+      ;; .getRGB method of BufferedImage returns a 24-bit integer
+      ;; add-pixel function decides where to add the pixel vector
+      (let [rgba (.getRGB image x y)
+            pixel (rgb-vector rgba)
+            add-pixel (if (= x (dec width))
+                              #(conj frame [%])
+                              #(conj (pop frame) (conj (peek frame) %)))]
+
+          (recur 0 (inc y) (add-pixel pixel)))))))
--- src/media/play.clj
+++ src/media/play.clj
@@ -2,30 +2,32 @@
   (:require
    [media.media :refer
     [get-frame get-format file-in decode-image gif-decoder]]
-   [char.characters :as char]
-   [clojure.pprint])
+   [char.characters :as char])
   (:import
    [java.awt.image BufferedImage]))
 
 (defn render-image
-  "Renders an ASCII image by printing a matrix row-wise."
-  [image color]
+  "Renders an ASCII image from a `BufferdImage` by calling `render` and prints the returned matrix row-wise."
+  [^BufferedImage image color]
+
   (let [frame (get-frame image)
         rendered-frame (if-not color
                          (char/render frame)
                          (char/render-with-color frame))]
+
     (doseq [row rendered-frame]
       (run! print row)
       (newline))))
 
 (defn render-gif
-  "Renders ASCII images from gif by looping through an ImageFileReader and letting images by index.
-  Images are rendered as a matrix and printed row-wise and calls to sleep and ansi-clear are sent."
+  "Returns ASCII images by looping through an ImageFileReader and letting images by index."
   ;; There is a bug in GIFImageReader: Index 4096 out of bounds for length 4096
   ;; This affects some gifs, alternative reader or patch is needed
-  [^com.sun.imageio.plugins.gif.GIFImageReader reader color]
-  (for [i (range (dec (.getNumImages reader true)))
-    :let [^BufferedImage image (.read reader i)
+  [^com.sun.imageio.plugins.gif.GIFImageReader gif-reader color]
+
+  (for [i (range (dec (.getNumImages gif-reader true)))
+
+    :let [^BufferedImage image (.read gif-reader i)
           frame (get-frame image)
           rendered-frame (if-not color
                            (char/render frame)
@@ -33,15 +35,26 @@
       rendered-frame))
 
 (defn play-gif
-  "Plays a gif by saving the rendered images with function `render-gif` in a vector and looping."
+  "Plays a GIFImageReader by extracting and rendering images with function `render-gif` and looping through the vectors.
+  Sleeps between frames and sends an ANSI terminal clear command to simulate video."
   [^com.sun.imageio.plugins.gif.GIFImageReader reader color]
+
   (let [images (render-gif reader color)]
+
+    ;; Main loop
     (while true
+
+      ;; Loop #1 vector of multiple images
       (doseq [i (range (dec (count images)))]
+
         (let [image (nth images i)]
+
+          ;; Loop #2 vector of rows of an image
           (doseq [row image]
             (run! print row)
             (newline)))
+
+        ;; Pause and clear outpu
         (Thread/sleep 100)
         (print (str \u001b \c))))))
 
@@ -51,15 +64,16 @@
   [filename & [color]]
 
   (if-let [^java.io.File file (file-in filename)]
+
     (let [format (get-format filename)]
+
       (cond
         (or (= format "png") (= format "jpg") (= format "bmp"))
-        (let
-            [^BufferedImage image (decode-image file)]
+        (let [^BufferedImage image (decode-image file)]
           (render-image image (or color false)))
         (= format "gif")
-        (let
-            [^com.sun.imageio.plugins.gif.GIFImageReader reader (gif-decoder file)]
+        (let [^com.sun.imageio.plugins.gif.GIFImageReader reader (gif-decoder file)]
           (play-gif reader (or color false)))
         :else (println "File not supported.")))
+
       "File could not be read."))
