01eeb2d26f9eaef8ab24017a3ce08289ad662063
derinsh <psisisu@gmail.com>
2022-12-22 05:40:09 +0100

    Help argument

--- src/main.clj
+++ src/main.clj
@@ -2,11 +2,17 @@
   (:gen-class :main true)
   (:require [media.play :refer [play]]))
 
+(def help-line "Run the program with a filename and the argument 'color' to print in color.")
+
 (defn -main
   "Generate and render an ASCII image of the provided file."
   [& args]
-  (let [filename (first args)
-        param (= "color" (first (rest args)))]
-    (if (>= 0 (count filename))
-      (throw (AssertionError. "No filename provided"))
-      (play filename param))))
+  (let [first-arg (first args)
+        second-arg (first (rest args))
+        help? (= "help" first-arg)
+        color? (= "color" second-arg)]
+    (if help?
+      (println help-line)
+      (if (>= 0 (count first-arg))
+        (throw (AssertionError. "No filename or argument provided"))
+        (play first-arg color?)))))
